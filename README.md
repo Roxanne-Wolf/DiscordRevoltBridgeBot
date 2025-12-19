b7f2ef65ad026be8b79f8466740fba9d5531aebe
Roxanne Wolf <104118584+Roxanne-Wolf@users.noreply.github.com>
2025-12-19 01:13:51 -0800

    Update for Stoat rebrand

--- README.md
+++ README.md
@@ -1,43 +1,46 @@
-# Discord+Revolt Bridge Bot
+# Discord+Stoat Bridge Bot
 
-Made by: roxanne.wolf. (On Discord) / RoxanneWolf#6117 (On Revolt.chat)
+Made by: roxanne.wolf. (On Discord) / RoxanneWolf#6117 (On Stoat.chat)
 
 ## About:
 
-This Discord+Revolt Bridge Bot source code allows you to connect your Discord and Revolt community together. This will allow your Discord and Revolt members to communicate with each other. This code is meant to be a simple and memory-efficient way to bridge your Discord and Revolt community.
+This Discord+Stoat Bridge Bot source code allows you to connect your Discord and Stoat community together. This will allow your Discord and Stoat members to communicate with each other. This code is meant to be a simple and memory-efficient way to bridge your Discord and Stoat community.
 
-This project will only work for 1 Discord and 1 Revolt server.
+This project will only work for 1 Discord and 1 Stoat server.
 
 ## Requirements:
 
 - Python 3.10 or newer.
 - Some basic knowledge of Python
-- A Discord AND revolt.chat bot
-- At least 256mb ram free and 512mb disk free.
+- A Discord AND stoat.chat bot
+- At least 512mb ram free and 512mb disk free.
 
 ## Setup:
 
 Here is the steps needed to get started with using this source code.
 
 1. Download/Fork this source code and upload it to your server
-2. Create a Discord and Revolt bot.
-- For Discord: Go to https://discord.com/developers/applications and create a new application. Under the bot section, enable the Message content intent so the bot can check the messages and send them to the Revolt channel set.
-- For Revolt: Go to https://app.revolt.chat/settings/bots and create a new bot. Make sure to copy the token.
+2. Create a Discord and Stoat bot.
+- For Discord: Go to https://discord.com/developers/applications and create a new application. Under the bot section, enable the Message content intent so the bot can check the messages and send them to the set Stoat channel.
+- For Stoat: Go to https://old.stoat.chat/settings/bots and create a new bot. Make sure to copy the token.
 3. Open config.json and fill in everything it asks for:
-- "discordbottoken": "" => Put in your Discord bot token here
-- "discordbotprefix": "!" => Put in the prefix for the Discord bot (not used)
-- "revoltbottoken": "" => Put in your Revolt.chat bot token.
-- "revoltbotprefix": "!" => Put in the prefix for the Revolt.chat bot.
-- "botname": "" => Enter the name you want to call your bot here (this name will show up on the embeds within the commands).
-- "discordchannelid": DISCORD_CHANNEL_ID_HERE => Replace DISCORD_CHANNEL_ID_HERE with your Discord channel ID
-- "discordwebhookurl": "" => Put in your Discord webhook url here.
-- "revoltchannelid": "" => Put in the Revolt channel ID here (Make sure the Revolt.chat bot has the "Use Masquerade" permission.
-- "lang": "" => Set the language code of the language. Defaults to English. (Refer to the supported languages section below)
-4. Invite your Discord and Revolt bot to your server
-- For the Discord bot, make sure it has the Read Messages permission on the channel you are trying to bridge to Revolt.
-- For the Revolt bot, make sure the bot has the "View Channel", "Send Messages", "Send Embeds", and "Use Masquerade" permissions (check channel overrides as well)
+```json
+{
+	"discordbottoken": "", #Put in your Discord bot token here
+	"discordbotprefix": "!", #Put in the prefix for the Discord bot (not used)
+	"stoatbottoken": "", #Put in your Stoat.chat bot token.
+	"stoatbotprefix": "!", #Put in the prefix for the Stoat.chat bot.
+	"discordchannelid": DISCORD_CHANNEL_ID_HERE, => Replace DISCORD_CHANNEL_ID_HERE with your Discord channel ID
+	"discordwebhookurl": "", => Put in your Discord webhook url here.
+	"stoatchannelid": "", => Put in the Stoat channel ID here (Make sure the Stoat.chat bot has the "Use Masquerade" permission.
+	"lang": "", => Set the language code of the language. Defaults to English. (Refer to the supported languages section below)
+}
+```
+4. Invite your Discord and Stoat bot to your servers
+- For the Discord bot, make sure it has the Read Messages permission on the channel you are trying to bridge to your Stoat channel.
+- For the Stoat bot, make sure the bot has the "View Channel", "Send Messages", "Send Embeds", and "Use Masquerade" permissions (check channel overrides as well)
 5. Install everything from the requirements.txt file.
-6. Run the main.py file. Assuming you did all steps correctly, your Discord+Revolt bridge bot should work.
+6. Run the main.py file. Assuming you did all steps correctly, your Discord+Stoat bridge bot should work.
 
 ## ToDo
 
@@ -54,7 +57,7 @@ Please open an issue or pull request if you have a solution to any of these issu
 - de = German
 - tr = Turkish
 
-| Language     | Verified translation? | Translation by (Revolt usernames)                  |
+| Language     | Verified translation? | Translation by (Stoat usernames)                   |
 |--------------|-----------------------|----------------------------------------------------|
 | English (en) | :white_check_mark:    | [RoxanneWolf#6117](https://gitlab.com/roxannewolf) |
 | Spanish (es) | :x:                   | Google Translate                                   |
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import discord
-import pyvolt
-from pyvolt.ext import commands
+import stoat
+from stoat.ext import commands
 import json
 import asyncio
 from discord import SyncWebhook
@@ -14,12 +14,11 @@ with open('config.json') as f:
 
 DISCORDBOTTOKEN = config.get('discordbottoken')
 DISCORDBOTPREFIX = config.get('discordbotprefix')
-REVOLTBOTTOKEN = config.get('revoltbottoken')
-REVOLTBOTPREFIX = config.get('revoltbotprefix')
-BOTNAME = config.get('botname')
+REVOLTBOTTOKEN = config.get('stoatbottoken')
+REVOLTBOTPREFIX = config.get('stoatbotprefix')
 DISCORDCHANNELID = config.get('discordchannelid')
 DISCORDWEBHOOKURL = config.get('discordwebhookurl')
-REVOLTCHANNELID = config.get('revoltchannelid')
+REVOLTCHANNELID = config.get('stoatchannelid')
 LANGSETTING = config.get('lang')
 
 def load_translations(lang=LANGSETTING):
@@ -33,46 +32,89 @@ def load_translations(lang=LANGSETTING):
 
 lang = LANGSETTING
 translation = load_translations(lang)
+stoatclient = commands.Bot(token=REVOLTBOTTOKEN, command_prefix=REVOLTBOTPREFIX)
 
-revoltclient = pyvolt.Client()
-revoltclient = commands.Bot(token=REVOLTBOTTOKEN, command_prefix=REVOLTBOTPREFIX)
-
-@revoltclient.on(pyvolt.MessageCreateEvent)
-async def on_message(event: pyvolt.MessageCreateEvent):
+@stoatclient.on(stoat.MessageCreateEvent)
+async def on_message(event: stoat.MessageCreateEvent):
     message = event.message
-    if message.author_id == revoltclient.state.my_id:
+    if message.author_id == stoatclient.state.my_id:
         return
     if message.channel.id == REVOLTCHANNELID:
         webhook = SyncWebhook.from_url(DISCORDWEBHOOKURL)
         webhook.send(f"{message.content}", username=f"{message.author.id}", avatar_url=f"{message.author.internal_avatar.attach_state(message.author.state, 'avatars').url()}", allowed_mentions=discord.AllowedMentions(everyone=False))
 
-@revoltclient.command()
+@stoatclient.command()
 async def help(ctx):
-    embed = pyvolt.SendableEmbed(title=f"{BOTNAME}", description=f"{REVOLTBOTPREFIX}help - {translation['help-helpd']}\n{REVOLTBOTPREFIX}ping - {translation['help-pingd']}\n{REVOLTBOTPREFIX}botinfo - {translation['help-botinfod']}\n{REVOLTBOTPREFIX}debug - {translation['help-debugd']}")
+    embed = stoat.SendableEmbed(title=f"{stoatclient.user.name}", description=f"{REVOLTBOTPREFIX}help - {translation['help-helpd']}\n{REVOLTBOTPREFIX}ping - {translation['help-pingd']}\n{REVOLTBOTPREFIX}botinfo - {translation['help-botinfod']}\n{REVOLTBOTPREFIX}debug - {translation['help-debugd']}\n{REVOLTBOTPREFIX}say\n{REVOLTBOTPREFIX}presence\n{REVOLTBOTPREFIX}status\n{REVOLTBOTPREFIX}statusreset")
     await ctx.channel.send(embeds=[embed])
 
-@revoltclient.command()
+@stoatclient.command()
 async def ping(ctx):
     await ctx.channel.send(translation['help-pingd'])
 
-@revoltclient.command()
+@stoatclient.command()
 async def botinfo(ctx):
-    embed = pyvolt.SendableEmbed(title=f"{BOTNAME} {translation['informationtext']}", description=f"{translation['botinfo-madeby']}: RoxanneWolf#6117\n{translation['botinfo-gitlabsource']}: https://gitlab.com/roxannewolf/discordrevoltbridgebot\n{translation['botinfo-codebergsource']}: https://codeberg.org/roxannewolf/DiscordRevoltBridgeBot\n\n{translation['botinfo-info']}")
+    embed = stoat.SendableEmbed(title=f"{stoatclient.user.name} {translation['informationtext']}", description=f"{translation['botinfo-madeby']}: RoxanneWolf#6117\n{translation['botinfo-gitlabsource']}: https://gitlab.com/roxannewolf/discordrevoltbridgebot\n{translation['botinfo-codebergsource']}: https://codeberg.org/roxannewolf/DiscordRevoltBridgeBot\n\n{translation['botinfo-info']}")
     await ctx.channel.send(embeds=[embed])
 
-@revoltclient.command()
+@stoatclient.command()
 async def debug(ctx, debugoption=""):
     if debugoption == "config":
         try:
-            embed = pyvolt.SendableEmbed(title=f"{BOTNAME} {translation['debug-title']}", description=f"{translation['debug-dchannel']}: {DISCORDCHANNELID}\n{translation['debug-rchannel']}: {REVOLTCHANNELID}\n{translation['debug-dbotprefix']}: {DISCORDBOTPREFIX}\n{translation['debug-rbotprefix']}: {REVOLTBOTPREFIX}")
+            embed = stoat.SendableEmbed(title=f"{stoatclient.user.name} {translation['debug-title']}", description=f"{translation['debug-dchannel']}: {DISCORDCHANNELID}\n{translation['debug-rchannel']}: {REVOLTCHANNELID}\n{translation['debug-dbotprefix']}: {DISCORDBOTPREFIX}\n{translation['debug-rbotprefix']}: {REVOLTBOTPREFIX}")
             await ctx.channel.send(embeds=[embed])
         except:
             await ctx.channel.send(translation['errormessage'])
     if debugoption == "troubleshoot":
-        embed = pyvolt.SendableEmbed(title=f"{BOTNAME} {translation['debug-title']}", description=f"{translation['debug-troubleshoot']}")
+        embed = stoat.SendableEmbed(title=f"{stoatclient.user.name} {translation['debug-title']}", description=f"{translation['debug-troubleshoot']}")
+        await ctx.channel.send(embeds=[embed])
+    if debugoption == "bot":
+        embed = stoat.SendableEmbed(title=f"{stoatclient.user.name} {translation['debug-title']}", description=f"Bot ID: {stoatclient.state.my_id}\nUsers: {len(stoatclient.users)}")
+        await ctx.channel.send(embeds=[embed])
+    if debugoption == "server":
+        embed = stoat.SendableEmbed(title=f"{stoatclient.user.name} {translation['debug-title']}", description=f"Server Name: {ctx.server.name}\nServer ID: {ctx.server.id}\nOwner ID: {ctx.server.owner_id}\nServer Discoverable?: {ctx.server.discoverable}")
         await ctx.channel.send(embeds=[embed])
     else:
         return
+
+@stoatclient.command()
+@commands.is_owner()
+async def say(ctx, *, message:str):
+    if any(word in message for word in blockedwords):
+        await ctx.send("Sorry but I will not say anything that is racist or promotes/encourages illegal activities")
+        return
+    await ctx.channel.send(message)
+
+@stoatclient.command()
+@commands.is_owner()
+async def presense(ctx, presencetype=""):
+    if presencetype == "online":
+        await stoatclient.http.edit_my_user(status=stoat.UserStatusEdit(presence=pyvolt.Presence.online))
+        await ctx.channel.send("Bot status has been changed")
+    if presencetype == "idle":
+        await stoatclient.http.edit_my_user(status=stoat.UserStatusEdit(presence=stoat.Presence.idle))
+        await ctx.channel.send("Bot status has been changed")
+    if presencetype == "focus":
+        await stoatclient.http.edit_my_user(status=stoat.UserStatusEdit(presence=stoat.Presence.focus))
+        await ctx.channel.send("Bot status has been changed")
+    if presencetype == "dnd":
+        await stoatclient.http.edit_my_user(status=stoat.UserStatusEdit(presence=stoat.Presence.busy))
+        await ctx.channel.send("Bot status has been changed")
+    if presencetype == "invisible":
+        await stoatclient.http.edit_my_user(status=stoat.UserStatusEdit(presence=stoat.Presence.invisible))
+        await ctx.channel.send("Bot status has been changed")
+
+@stoatclient.command()
+@commands.is_owner()
+async def status(ctx, *, statustext):
+    await stoatclient.http.edit_my_user(status=stoat.UserStatusEdit(text=f"{statustext}"))
+    await ctx.channel.send("Bot status has been changed")
+
+@stoatclient.command()
+@commands.is_owner()
+async def statusreset(ctx):
+    await stoatclient.http.edit_my_user(status=stoat.UserStatusEdit(text=None))
+    await ctx.channel.send("Bot status has been reset")
 
 
 
@@ -90,18 +132,21 @@ class DiscordBot(discord.Client):
         if message.webhook_id:
             return
         if message.channel.id == DISCORDCHANNELID:
-            await revoltclient.http.send_message(REVOLTCHANNELID, f"{message.content}", masquerade=pyvolt.MessageMasquerade(name=f"{message.author.name}", avatar=f"{message.author.avatar}"))
+            await stoatclient.http.send_message(REVOLTCHANNELID, f"{message.content}", masquerade=stoat.MessageMasquerade(name=f"{message.author.name}", avatar=f"{message.author.avatar}"))
 
 
 
 
+
+if "__main__" == __name__:
+    with open("blockedwords.txt", "r") as f:
+        blockedwords = f.read().splitlines()
 
 async def main():
-    pyvolt.utils.setup_logging(root=True)
     discordclient = DiscordBot()
     await asyncio.gather(
         discordclient.start(DISCORDBOTTOKEN),
-        revoltclient.start(),
+        stoatclient.start(),
     )
 
 asyncio.run(main())
